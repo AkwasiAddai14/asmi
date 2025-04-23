267dcbf4f1660b297a7b5e3084417fc709aec5d5
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-04-23 22:53:34 +0200

    asmi

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,16 @@
+/* import { Providers } from "@/app/providers";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <>
+  <Providers>
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+      <div className="flex items-center justify-center w-full">
+        <SignUp path="/sign-up"/>
+      </div>
+    </div>
+    </Providers>
+    </>
+)
+} */
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,9 +12,11 @@ import {
   PopoverPanel,
 } from '@headlessui/react'
 import clsx from 'clsx'
-
+// import { useRouter } from 'next/navigation'
 import { Container } from '@/components/Container'
 import avatarImage from '@/images/avatar.jpg'
+import router from 'next/router'
+
 
 function CloseIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -45,7 +47,7 @@ function ChevronDownIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-function SunIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+/* function SunIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
     <svg
       viewBox="0 0 24 24"
@@ -75,7 +77,27 @@ function MoonIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
       />
     </svg>
   )
+} */
+
+function ProfileIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      fill="none"
+      aria-hidden="true"
+      {...props}
+    >
+      {/* Hoofd */}
+      <circle cx="12" cy="8" r="4" />
+      {/* Schouders */}
+      <path d="M4 20c0-4 4-6 8-6s8 2 8 6" />
+    </svg>
+  )
 }
+
 
 function MobileNavItem({
   href,
@@ -149,13 +171,13 @@ function NavItem({
         className={clsx(
           'relative block px-3 py-2 transition',
           isActive
-            ? 'text-teal-500 dark:text-teal-400'
-            : 'hover:text-teal-500 dark:hover:text-teal-400',
+            ? 'text-sky-500 dark:text-sky-400'
+            : 'hover:text-sky-500 dark:hover:text-sky-400',
         )}
       >
         {children}
         {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-linear-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+          <span className="absolute inset-x-1 -bottom-px h-px bg-linear-to-r from-teal-500/0 via-sky-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-sky-400/40 dark:to-sky-400/0" />
         )}
       </Link>
     </li>
@@ -190,10 +212,10 @@ function ThemeToggle() {
       type="button"
       aria-label={mounted ? `Switch to ${otherTheme} theme` : 'Toggle theme'}
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
-      onClick={() => setTheme(otherTheme)}
+      onClick={() => router.push('../sign-in')}
     >
-      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400" />
+      <ProfileIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-sky-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-sky-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
+      {/* <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400" /> */}
     </button>
   )
 }
@@ -248,6 +270,7 @@ function Avatar({
 }
 
 export function Header() {
+
   let isHomePage = usePathname() === '/'
 
   let headerRef = useRef<React.ElementRef<'div'>>(null)
