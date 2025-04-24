a69362bc49c789975508b6458cd5ec77d4023fd5
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-04-24 13:04:35 +0200

    asmi

--- functions-asmi/next.config.js
+++ functions-asmi/next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'standalone'
-  };
-  
-  module.exports = nextConfig;
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.production' });
+
+module.exports = {
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
+  }
+};
   
