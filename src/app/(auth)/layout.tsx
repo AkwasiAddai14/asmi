8912d74d497ac0f4cde272fc8393ac139326fe01
AkwasiAddai14 <akwasivdsm@gmail.com>
2025-04-24 15:19:29 +0200

    asmi

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,30 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import '@/styles/tailwind.css'
+import {  nlNL, } from '@clerk/localizations'
+import { Providers } from '../providers'
+
+
+export const metadata = {
+  title: 'Junter',
+  description: 'Empowering progress, enabling growth.'
+}
+
+const inter = Inter({subsets: ["latin"]})
+
+export default function AuthLayout({children} : {children : React.ReactNode}){
+  return(
+    <ClerkProvider 
+    localization={nlNL}
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+       <body className={`${inter.className} bg-dark-1`}>
+        <Providers>
+        {children}
+        </Providers>
+       </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,16 @@
+import { Providers } from "@/app/providers";
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <>
+ <Providers>
+      <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+      <div className="flex items-center justify-center w-full">
+          <SignIn path="/sign-in" />
+        </div>
+      </div>
+      </Providers>
+    </>
+  );
+}  
